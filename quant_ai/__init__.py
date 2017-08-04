bf4f183861ed98b3465eaa2607dac415db9c5a1f
andrewresnikoff <Andrew@MacBook-Pro-3.local>
2017-08-03 21:54:50 -0400

    began env setup

---
+++ quant_ai/__init__.py
@@ -0,0 +1,11 @@
+from gym.envs.registration import register
+
+register(
+    id='quant_ai-v0',
+    entry_point='quant_ai.envs:QuantAIEnv',
+)
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'quant_ai',
+	  version = '0.0.1',
+	  install_requires=['gym']
+)
+
+
