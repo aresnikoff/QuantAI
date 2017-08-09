ad0d7a631a58de81402da59c1a5891581cec3d94
Andrew Resnikoff <andrewresnikoff@gmail.com>
2017-08-09 14:02:14 -0400

    update pipeline.py

--- data/pipelines.py
+++ data/pipelines.py
@@ -2,16 +2,11 @@ from zipline.pipeline import Pipeline, CustomFactor
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.factors import (
 
-	### EXISTING FACTOR IMPORTS GO HERE
-
-	Returns,
-	AverageDollarVolume,
-	SimpleMovingAverage
+	### ADD ZIPLINE FACTORS HERE
 )
 
 from factors import *
 
-### CUSTOM PIPELINES GO HERE
 
 def sample_pipeline():
 
@@ -24,58 +19,21 @@ def sample_pipeline():
 	return Pipeline(columns = columns, screen = universe), len(columns)
 
 
-def recent_prices():
-
-	price_filter = USEquityPricing.close.latest >= 5
-	high_dollar_volume = AverageDollarVolume(window_length = 5).top(500)
-	universe = price_filter & high_dollar_volume
-
-	columns = {}
-	columns["Last"] = USEquityPricing.close.latest
-	columns["Returns"] = Returns(window_length = 2)
-	columns["Returns_10"] = Returns(window_length = 10)
-
-	for i in range(2, 10):
-		name = "close_" + str(i)
-		val = CloseOnN(window_length = i)
-		columns[name] = val
-
-	return Pipeline(columns = columns, screen = universe), len(columns)
-
-def momentum():
-	price_filter = USEquityPricing.close.latest >= 5
-	high_dollar_volume = AverageDollarVolume(window_length = 5).top(1500)
-	universe = price_filter & high_dollar_volume
-
-	columns = {}
-	columns["Last"] = USEquityPricing.close.latest
-	columns["Returns"] = Returns(window_length = 2)
-	columns["SMA_2"] = SimpleMovingAverage(window_length = 2)
-	columns["SMA_10"] = SimpleMovingAverage(window_length = 10)
-
-	return Pipeline(columns = columns, screen = universe), len(columns)
+### CREATE PIPELINE HERE
 
 
-""" 
-ADD YOUR PIPELINE TO THE PIPELINE LIST 
-IN ORDER TO BRING IT INTO YOUR ALGORITHM
-"""
+
+### ADD PIPELINE CODES HERE
 
 __PIPELINE_LIST__ = {
 	
-	"sample_pipeline": sample_pipeline,
-	"momentum": momentum
+	"sample_pipeline": sample_pipeline
 
 }
 
 def get_pipeline(code):
 
 	return __PIPELINE_LIST__[code]()
-
-
-
-
-
 
 
 
